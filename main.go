9e0d89b14a9212dd3db9fbe1a0064b1bf2b64cfa
pindarEng <ursalb11@gmail.com>
2024-03-24 20:40:49 +0200

    go-chi routing handling

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"html/template"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello project!")
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", basicHandler)
+	router.Get("/", dashboardHandler)
+
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("failed to listen to server", err)
+	}
+}
+
+func dashboardHandler(w http.ResponseWriter, r *http.Request) {
+	// Parse the HTML template
+	tmpl, err := template.ParseFiles("templates/dashboard.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Execute the template, passing any necessary data (nil in this case)
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, world!"))
 }
